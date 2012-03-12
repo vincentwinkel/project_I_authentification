e55b452592e9972d3ed03bfcc83fa000041ed1c5
Vincent Winkel <winkel@live.fr>
2012-03-12 21:59:19 +0100

    Modif de spec

--- sauth/spec/sauth_spec.rb
+++ sauth/spec/sauth_spec.rb
@@ -7,9 +7,13 @@ def app
 	Sinatra::Application;
 end
 
+#last_response.status.should be_redirect;
+#follow_redirect!;
+#last_request.path.should == "/path";
+
 describe "Without session" do
 	before(:each) do
-		#session={};
+		User.all.each { |u| User.delete(u.id); }
 	end
 	#############################
 	describe "Default page" do
@@ -17,7 +21,8 @@ describe "Without session" do
 		it "should print the default page with a wrong URL" do
 			get "/wrong_url";
 			last_response.status.should == 302;
-			last_response.header["Location"].should == "http://example.org/sessions/new";
+			follow_redirect!;
+			last_request.path.should == "/sessions/new";
 		end
 	end
 	#############################
@@ -69,21 +74,23 @@ describe "Without session" do
 		end
 		it "should print again this page if an error occurred (account already exists)" do
 			u=User.new;
-			u.login="Vincent";
+			u.login="login_test";
 			u.password="mdp";
 			u.save;
 			tmp_id=u.id;
-			params={"login" => "Vincent","password" => "mdp","password_confirmation" => "mdp"};
+			params={"login" => "login_test","password" => "mdp","password_confirmation" => "mdp"};
 			post "/register", params;
 			last_response.status.should == 200;
 			settings.form_errors.should == {"login" => settings.ERROR_FORM_BAD_LOGIN};
 			User.delete(tmp_id);
 		end
 		it "should redirect to the protected area (any error)" do
-			params={"login" => "Vincent","password" => "mdp","password_confirmation" => "mdp"};
+			params={"login" => "login_test","password" => "mdp","password_confirmation" => "mdp"};
 			post "/register", params;
 			last_response.status.should == 302;
-			last_response.header["Location"].should == "http://example.org/protected";
+			follow_redirect!;
+			last_request.path.should == "/protected";
+			last_request.session["suser"].should == "login_test";
 		end
 	end
 	#############################
@@ -122,14 +129,16 @@ describe "Without session" do
 		end
 		it "should redirect to the protected area (any error)" do
 			u=User.new;
-			u.login="Vincent";
+			u.login="login_test";
 			u.password="mdp";
 			u.save;
 			tmp_id=u.id;
-			params={"login" => "Vincent","password" => "mdp"};
+			params={"login" => "login_test","password" => "mdp"};
 			post "/sessions/new", params;
 			last_response.status.should == 302;
-			last_response.header["Location"].should == "http://example.org/protected";
+			follow_redirect!;
+			last_request.path.should == "/protected";
+			last_request.session["suser"].should == "login_test";
 			User.delete(tmp_id);
 		end
 	end
@@ -137,9 +146,93 @@ describe "Without session" do
 	describe "Deconnection page" do
 	#############################
 		it "should print the connection page" do
-			get "/sessions/id";
+			get "/sessions/destroy";
 			last_response.status.should == 302;
-			last_response.header["Location"].should == "http://example.org/sessions/new";
+			follow_redirect!;
+			last_request.path.should == "/sessions/new";
+			last_request.session["suser"].should be_nil;
+		end
+	end
+	#############################
+	describe "Protected area" do
+	#############################
+		it "should redirect to the connection page" do
+			get "/protected";
+			last_response.status.should == 302;
+			follow_redirect!;
+			last_request.path.should == "/sessions/new";
+			last_request.session["suser"].should be_nil;
+		end
+	end
+end
+
+
+describe "With session" do
+	before(:each) do
+		User.all.each { |u| User.delete(u.id); }
+		u=User.new;
+		u.login="login_test";
+		u.password="mdp";
+		u.save;
+		tmp_id=u.id;
+		params={"login" => "login_test","password" => "mdp","password_confirmation" => "mdp"};
+		post "/register", params;
+		User.delete(tmp_id);
+		#app.stub(:is_connected) { true };
+	end
+	#############################
+	describe "Default page" do
+	#############################
+		it "should print the default page with a wrong URL" do
+			get "/wrong_url";
+			last_response.status.should == 302;
+			follow_redirect!;
+			last_request.path.should == "/sessions/new";
+			print "#####"+last_response.inspect+"\n\n\n\n";
+			print "#####"+last_request.inspect+"\n\n\n\n";
+			last_request.session["suser"].should == "login_test";
+		end
+	end
+	#############################
+	describe "Inscription page" do
+	#############################
+		it "should redirect to the protected area" do
+			get "/register";
+			last_response.status.should == 302;
+			follow_redirect!;
+			last_request.path.should == "/protected";
+			last_request.session["suser"].should == "login_test";
+		end
+	end
+	#############################
+	describe "Connection page" do
+	#############################
+		it "should redirect to the protected area" do
+			get "/sessions/new";
+			last_response.status.should == 302;
+			follow_redirect!;
+			last_request.path.should == "/protected";
+			last_request.session["suser"].should == "login_test";
+		end
+	end
+	#############################
+	describe "Deconnection page" do
+	#############################
+		it "should redirect to the protected area" do
+			get "/sessions/destroy";
+			last_response.status.should == 302;
+			follow_redirect!;
+			last_request.path.should == "/sessions/new";
+			last_request.session["suser"].should be_nil;
+		end
+	end
+	#############################
+	describe "Protected area" do
+	#############################
+		it "should print the page" do
+			get "/protected";
+			last_response.status.should == 200;
+			last_request.session["suser"].should == "login_test";
 		end
 	end
 end
